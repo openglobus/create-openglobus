a1259cd45bd1b3aa3d8fba1c9c8553a7fc566ad5
pavletto <pvlt.job@gmail.com>
2023-05-05 19:14:56 +0400

    create package

---
+++ src/templates/template-openglobus/main.js
@@ -0,0 +1,1 @@
+import globus from './globus.js'
---
+++ src/utils/template.ts
@@ -0,0 +1,7 @@
+import ejs from 'ejs';
+export interface TemplateData {
+    projectName: string
+}
+export function render(content: string, data: TemplateData) {
+    return ejs.render(content, data);
+}
