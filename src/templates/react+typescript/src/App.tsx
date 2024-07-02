9a81596962638db80eb17d988c3d3ebf687de354
Pavel <pvlt.job@gmail.com>
2024-07-02 11:47:38 +0400

    fix react template

--- src/templates/react+typescript/src/App.tsx
+++ src/templates/react+typescript/src/App.tsx
@@ -1,16 +1,41 @@
 import '@openglobus/openglobus-react/dist/style.css'
+import './App.css'
 import {Entity, Globe, GlobeContextProvider, Label, Vector} from '@openglobus/openglobus-react'
 
 
 function App() {
     return <GlobeContextProvider>
         <Globe fontsSrc="https://openglobus.org/examples/examples/fonts/fonts">
-            <Vector name="test">
+            <Vector name="test" >
                 <Entity
                     alt={0}
                     lat={0}
                     lon={0}
                     name="Custom Entity"
+                >
+                    <Label
+                        align="center"
+                        color="#053df3"
+                        face="Audiowide-Regular"
+                        offset={[
+                            0,
+                            32,
+                            0
+                        ]}
+                        opacity={1}
+                        outline={0.2}
+                        outlineColor="#33fa07"
+                        rotation={0}
+                        size={32}
+                        text="Hello world!"
+                    />
+
+                </Entity>
+                <Entity
+                    alt={0}
+                    lat={0}
+                    lon={0}
+                    name="Custom q"
                 >
                     <Label
                         align="center"
@@ -25,10 +50,10 @@ function App() {
                         outline={0.2}
                         outlineColor="#33fa07"
                         rotation={0}
-                        size={32}
-                        text="Hello, World!"
-                        visibility
+                        size={24}
+                        text="Welcome to OpenGlobus!"
                     />
+
                 </Entity>
             </Vector>
         </Globe>
