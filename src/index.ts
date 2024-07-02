43d906f2242acacdc57f42dc5ccb4afd4c9d9ce2
Pavel <pvlt.job@gmail.com>
2024-07-02 11:06:58 +0400

    remove chalk

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@
 import fs from 'fs';
 import path, {dirname} from 'path';
 import inquirer from 'inquirer';
-import chalk from 'chalk';
 import shell from 'shelljs';
 import {fileURLToPath} from 'url';
 import ejs from 'ejs';
@@ -63,7 +62,7 @@ inquirer.prompt(QUESTIONS).then(answers => {
 
 function createProject(projectPath: string) {
     if (fs.existsSync(projectPath)) {
-        console.log(chalk.red(`Folder ${projectPath} exists. Delete or use another name.`));
+        console.log(`Folder ${projectPath} exists. Delete or use another name.`);
         return false;
     }
     fs.mkdirSync(projectPath);
