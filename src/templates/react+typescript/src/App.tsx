c20678a476214eb756dddb39c0acd52758318140
Pavel <pvlt.job@gmail.com>
2024-07-02 10:58:23 +0400

    update dependencies and update templates to new version og and react lib

--- src/templates/react+typescript/src/App.tsx
+++ src/templates/react+typescript/src/App.tsx
@@ -1,13 +1,38 @@
 import '@openglobus/openglobus-react/dist/style.css'
-import Globus, {GlobusContextProvider} from '@openglobus/openglobus-react'
-import ButtonContainer from "./ButtonContainer.tsx";
+import {Entity, Globe, GlobeContextProvider, Label, Vector} from '@openglobus/openglobus-react'
 
 
 function App() {
-    return <GlobusContextProvider>
-        <Globus/>
-        <ButtonContainer/>
-    </GlobusContextProvider>
+    return <GlobeContextProvider>
+        <Globe fontsSrc="https://openglobus.org/examples/examples/fonts/fonts">
+            <Vector name="test">
+                <Entity
+                    alt={0}
+                    lat={0}
+                    lon={0}
+                    name="Custom Entity"
+                >
+                    <Label
+                        align="center"
+                        color="#053df3"
+                        face="Audiowide-Regular"
+                        offset={[
+                            0,
+                            0,
+                            0
+                        ]}
+                        opacity={1}
+                        outline={0.2}
+                        outlineColor="#33fa07"
+                        rotation={0}
+                        size={32}
+                        text="Hello, World!"
+                        visibility
+                    />
+                </Entity>
+            </Vector>
+        </Globe>
+    </GlobeContextProvider>
 }
 
 export default App
